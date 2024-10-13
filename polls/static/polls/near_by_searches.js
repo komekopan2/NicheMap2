7032fe66a42de50c2c3a6e3988425ce156384600
komekopan2 <yoo.shimizu888@gmail.com>
2024-10-13 18:57:19 +0900

    プレイスライブラリのnear_by_searchesとplace_detailsの統合3

--- polls/static/polls/near_by_searches.js
+++ polls/static/polls/near_by_searches.js
@@ -23,7 +23,7 @@ function initMap() {
         for (var i = 0; i < results.length; i++) {
             var getDetailsRequest = {
                 placeId: results[i].place_id,
-                fields: ["name", "formatted_address", "place_id", "geometry"],
+                fields: ["name", "photo","business_status","formatted_address", "geometry", "rating", "user_ratings_total", "url"],
             };
             // Place detail Windowを複数にするため、ここで宣言
             // const infowindow = new google.maps.InfoWindow();
@@ -36,23 +36,55 @@ function initMap() {
                 ) {
                     const marker = new google.maps.Marker({
                         map,
+                        icon: place.photos[0].getUrl({maxWidth: 35, maxHeight: 35}),
                         position: place.geometry.location,
                     });
 
                     google.maps.event.addListener(marker, "click", () => {
                         const content = document.createElement("div");
 
+                        const imageElement = document.createElement("img");
+                        imageElement.src = place.photos[0].getUrl({maxWidth: 200, maxHeight: 200});
+                        content.appendChild(imageElement);
+
                         const nameElement = document.createElement("h2");
                         nameElement.textContent = place.name;
                         content.appendChild(nameElement);
 
-                        const placeIdElement = document.createElement("p");
-                        placeIdElement.textContent = place.place_id;
-                        content.appendChild(placeIdElement);
+                        // business_statusがOPERAIONALの場合は「営業中」、CLOSED_TEMPORARILYの場合は「一時休業中」、CLOSED_PERMANENTLYの場合は「閉店済み」と表示
+                        const placeBusinessStatusElement = document.createElement("p");
+                        if (place.business_status === "OPERATIONAL") {
+                            placeBusinessStatusElement.textContent = "営業中";
+                        }
+                        if (place.business_status === "CLOSED_TEMPORARILY") {
+                            placeBusinessStatusElement.textContent = "一時休業中";
+                        }
+                        if (place.business_status === "CLOSED_PERMANENTLY") {
+                            placeBusinessStatusElement.textContent = "閉店済み";
+                        }
+                        content.appendChild(placeBusinessStatusElement);
 
+                        // 住所を表示
                         const placeAddressElement = document.createElement("p");
                         placeAddressElement.textContent = place.formatted_address;
                         content.appendChild(placeAddressElement);
+
+                        // 評価を表示
+                        const placeRatingElement = document.createElement("p");
+                        placeRatingElement.textContent = place.rating;
+                        content.appendChild(placeRatingElement);
+
+                        // レビュー数を表示
+                        const placeRatingsTotalElement = document.createElement("p");
+                        placeRatingsTotalElement.textContent = place.user_ratings_total;
+                        content.appendChild(placeRatingsTotalElement);
+
+                        // 「Google Mapsで見る」リンクを挿入し、新しいタブで開くようにする
+                        const placeUrlElement = document.createElement("a");
+                        placeUrlElement.href = place.url;
+                        placeUrlElement.target = "_blank";
+                        placeUrlElement.textContent = "Google Mapsで見る";
+                        content.appendChild(placeUrlElement);
 
                         infowindow.setContent(content);
                         infowindow.open(map, marker);
