d214b39cfda214938cee9c1c8c746cc8fef0c04b
komekopan2 <yoo.shimizu888@gmail.com>
2024-10-13 15:24:44 +0900

    プレイスライブラリのnear_by_searchesチュートリアル完了3

--- polls/static/polls/near_by_searches.js
+++ polls/static/polls/near_by_searches.js
@@ -34,46 +34,47 @@ var infowindow;
 // Nearby Search リクエスト
 ///////////////////////////////////////////////////////////
 function initMap() {
-  var pyrmont = new google.maps.LatLng(35.16363182,136.90907037);
+    var pyrmont = new google.maps.LatLng(35.16363182, 136.90907037);
 
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 15
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pyrmont,
+        zoom: 15
     });
 
-  var request = {
-    location: pyrmont,
-    radius: '500',
-    type: ['レストラン','カフェ'],
-    rankBy: google.maps.places.RankBy.PROMINENCE
-  };
+    var request = {
+        location: pyrmont,
+        radius: '500',
+        type: ['レストラン', 'カフェ'],
+        rankBy: google.maps.places.RankBy.PROMINENCE
+    };
 
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
 }
 
 function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
     }
-  }
 }
+
 ///////////////////////////////////////////////////////////
 // 下記はplace_searchライブラリで共通なので変えない
 ///////////////////////////////////////////////////////////
 function createMarker(place) {
-  if (!place.geometry || !place.geometry.location) return;
+    if (!place.geometry || !place.geometry.location) return;
 
-  const marker = new google.maps.Marker({
-    map,
-    position: place.geometry.location,
-  });
+    const marker = new google.maps.Marker({
+        map,
+        position: place.geometry.location,
+    });
 
-  google.maps.event.addListener(marker, "click", () => {
-    infowindow.setContent(place.name || "");
-    infowindow.open(map);
-  });
+    google.maps.event.addListener(marker, "click", () => {
+        infowindow.setContent(place.name || "");
+        infowindow.open(map);
+    });
 }
 
 window.initMap = initMap;
